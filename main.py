221fbfb701dfaf1517151012d9ab5a263694d823
XRplayer765 <mathew.hobday@gmail.com>
2024-09-02 15:28:21 +0100

    adding asteroidfields class to allow asteroids to spawn and move by themselves

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -15,8 +17,12 @@ def main():
     y = SCREEN_HEIGHT / 2
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player_instance = Player(x, y)
+    asteroid_field_instance = AsteroidField()
     
     #game loop starts
     while True:
