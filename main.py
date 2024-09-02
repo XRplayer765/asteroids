0b33ada4f69f78b9a1be06760f0d7b07202d8c83
XRplayer765 <mathew.hobday@gmail.com>
2024-09-02 13:05:52 +0100

    adding player and circleshape module, updating constants and main to draw player sprite

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player_instance = Player(x, y)
 
     while True:
         for event in pygame.event.get():
@@ -15,7 +21,9 @@ def main():
                 return
         
         screen.fill("black")
+        player_instance.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
