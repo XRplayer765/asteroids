8707a916b3d84836f0ed4deb068a79c8fc23dee2
XRplayer765 <mathew.hobday@gmail.com>
2024-09-02 13:07:39 +0100

    adding player and circleshape module, updating constants and main to draw player sprite

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -19,3 +19,6 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation + (PLAYER_TURN_SPEED * dt)
